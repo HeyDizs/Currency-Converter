3c3528cfe785fb9a1116f184083b8fde6e53e3be
David Adesanya Ejaria <davidadeola2006@gmail.com>
2026-09-19 22:21:10 +0100

    update the app.py to receicve all currency and also the frontend

--- app.py
+++ app.py
@@ -2,11 +2,24 @@ import requests
 from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
-API_URL = "https://er-api.com"
+API_KEY = '218fcda7c311cd6a179bce1d'
+API_URL = f"https://er-api.com?apikey={API_KEY}"
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/get_supported_currencies")
+def get_supported_currencies():
+    try:
+        response = requests.get(f"{API_URL}/supported", timeout=10)
+        response.raise_for_status()
+        data = response.json()
+        return jsonify(data)
+    except Exception as e:
+        return jsonify({"Error": "Failed to fetch rate", "details": str(e)}), 500
+
+
 
 @app.route("/get_rate/<base_currency>")
 def get_rate(base_currency):
