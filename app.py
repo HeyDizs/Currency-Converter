22aadd6e62564c1dccf0c12bcd97fe3b0efd4c6c
David Adesanya Ejaria <davidadeola2006@gmail.com>
2026-09-20 10:39:15 +0100

    Updated it again

--- app.py
+++ app.py
@@ -2,34 +2,33 @@ import requests
 from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
-API_KEY = '218fcda7c311cd6a179bce1d'
-API_URL = f"https://er-api.com?apikey={API_KEY}"
+
+API_URL = "https://open.er-api.com/v6/latest"
+
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
-@app.route("/get_supported_currencies")
-def get_supported_currencies():
-    try:
-        response = requests.get(f"{API_URL}/supported", timeout=10)
-        response.raise_for_status()
-        data = response.json()
-        return jsonify(data)
-    except Exception as e:
-        return jsonify({"Error": "Failed to fetch rate", "details": str(e)}), 500
 
-
-
-@app.route("/get_rate/<base_currency>")
+@app.route("/get_rates/<base_currency>")
 def get_rate(base_currency):
     try:
-        response = requests.get(f"{API_URL}?from{base_currency}", timeout=10)
+        response = requests.get(
+            f"{API_URL}/{base_currency}",
+            timeout=10
+        )
+
         response.raise_for_status()
-        data = response.json()
-        return jsonify(data)
+
+        return jsonify(response.json())
+
     except Exception as e:
-        return jsonify({"Error": "Failed to fetch rate", "details": str(e)}), 500
-    
-if __name__ == "main":
+        return jsonify({
+            "error": "Failed to fetch rates",
+            "details": str(e)
+        }), 500
+
+
+if __name__ == "__main__":
     app.run(debug=True)
