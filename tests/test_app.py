8771678ae589ff70a8376e6dfcc3758bcc7cee2a
Chidimma Nnaji <nnajichidimma16@gmail.com>
2026-09-22 09:48:51 +0100

    test: add currency rates endpoint test

--- tests/test_app.py
+++ tests/test_app.py
@@ -7,3 +7,10 @@ def test_homepage():
     response = client.get("/")
 
     assert response.status_code == 200
+
+def test_get_rates():
+    client = app.test_client()
+
+    response = client.get("/get_rates/USD")
+
+    assert response.status_code == 200
